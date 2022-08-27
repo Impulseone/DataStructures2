374d707cb86b6a9197efa84a73506a3d3ead8345
Sergei <sk_developer@mail.ru>
2022-08-27 13:10:48 +0300

    AlgorithmsDataStructures2

--- src/AlgorithmsDataStructures2/AlgorithmsDataStructures2Test.java
+++ src/AlgorithmsDataStructures2/AlgorithmsDataStructures2Test.java
@@ -1,7 +1,19 @@
 package AlgorithmsDataStructures2;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class AlgorithmsDataStructures2Test {
-
+    @Test
+    public void testGenerateBBSTArray() {
+        int[] resultArray = AlgorithmsDataStructures2.GenerateBBSTArray(new int[]{1, 2, 3, 4, 5});
+        assertEquals(3, resultArray[0]);
+        assertEquals(2, resultArray[1]);
+        assertEquals(5, resultArray[2]);
+        assertEquals(1, resultArray[3]);
+        assertEquals(0, resultArray[4]);
+        assertEquals(4, resultArray[5]);
+        assertEquals(0, resultArray[6]);
+    }
 }
